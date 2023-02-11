4fb01c2996d932688843316c42e7e4dd6bc9f319
razzuSharma <razzusharma008@gmail.com>
2023-02-11 10:31:26 +0545

    editing the routes

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Login from "./pages/Login";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 import { Route, Routes } from "react-router-dom";
+import ViewProduct from "./pages/ViewProduct";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/login" element={<Login />} />
       <Route path="/product" element={<Product />} />
+      <Route path="/viewProduct/:id" element={<ViewProduct/>} />
     </Routes>
   );
 }
--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,9 +1,11 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 function Product() {
-  const productApi = "https://fakestoreapi.com/products";
+  const productApi = "http://localhost:3000/posts ";
   const [productData, setProductData] = useState([]);
+
   function getDataProducts() {
     axios.get(productApi).then((res) => setProductData(res.data));
   }
@@ -13,29 +15,41 @@ function Product() {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 h-fit">
       {productData.map((value) => {
-        const { id, title, price, description, category, image } = value;
+        const { id, title, price, image } = value;
         return (
           <div key={id}>
-            <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <div className="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
               <a href="#">
-                <img class="p-8 rounded-t-lg" src={image} alt="product image" />
+                <img
+                  className="p-8 rounded-t-lg"
+                  src={image}
+                  alt="product image"
+                />
               </a>
-              <div class="px-5 pb-5">
+              <div className="px-5 pb-5">
                 <a href="#">
-                  <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
+                  <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
                     {title}
                   </h5>
                 </a>
-                <div class="flex items-center justify-between">
-                  <span class="text-3xl font-bold text-gray-900 dark:text-white">
+                <div className="flex items-center justify-between">
+                  <span className="text-3xl font-bold text-gray-900 dark:text-white">
                     {price}
                   </span>
-                  <a
-                    href="#"
-                    class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                   <Link to=
+                   {{
+                    pathname: `/viewProduct/${id}`,
+                    state:value
+                   }} className="text-white m-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    View
+                    </Link>
+
+                  {/* <button
+                    onClick={openProduct}
+                    className="text-white m-2 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                   >
-                    Add to cart
-                  </a>
+                    View
+                  </button> */}
                 </div>
               </div>
             </div>
